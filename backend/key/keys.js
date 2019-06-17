bd9edceda5fc03ecf54399e3b2485dc7ae71d334
Animesh Grover <animesh70@gmail.com>
2019-06-16 19:10:31 -0700

    added initial temp payment service for complete purchase flow.

--- backend/key/keys.js
+++ backend/key/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     secretOrKey: 'secret',
-    sendgridApiKey:'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'
+    sendgridApiKey: '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',
+    stripeApiKey: 'sk_test_XipJxj5aXqXxdWljrsx5PKKn00MHy6FWuY'
 };
