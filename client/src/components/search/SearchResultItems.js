612483a6b9f8cc6f74f771b630e902e7698ee76d
Matthew Kwong <mtkwong91@gmail.com>
2019-06-27 09:12:47 -0700

    show message if no search results found

--- client/src/components/search/SearchResultItems.js
+++ client/src/components/search/SearchResultItems.js
@@ -20,35 +20,47 @@ class SearchResultItems extends Component {
   }
 
   render() {
-    return (
-      <ul class="search-item-list">
-        {this.props.items.map(item => {
-          return (
-            <li key={item.item_id} class="search-item row">
-              <div class="col-2">
-                <Link to={"/items/" + item.item_id}>
-                  <img onClick={() => window.scrollTo(0, 0)} 
-                       class="search-item-img" style={{width: "100%"}} src={item.item_image} alt="Item"></img>
-                </Link>
-              </div>
-              <div class="col-6">
-                <Link to={"/items/" + item.item_id}>
-                  <div onClick={() => window.scrollTo(0, 0)}>{item.item_name}</div>
-                </Link>
-                <div>{item.description}</div>
-                <div>{this.renderRating(item.average_rating)}</div>
-              </div>
-              <div class="col-1">
-                <div>{"$" + item.price}</div>
-              </div>
-              <div class="col-2">
-                <CartAddButton item={item} cls={"addtocart-btn-lg"}/>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
-    );
+    if (this.props.items.length === 0) {
+      return (
+        <div className="sr-outer-div">
+          <span class="sr-no-results">Sorry, no results found.</span>
+          <br/>
+          <Link to="/product">
+            <button onClick={() => window.scrollTo(0, 0)} className="sr-shop-button">Browse Items</button>
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <ul class="search-item-list">
+          {this.props.items.map(item => {
+            return (
+              <li key={item.item_id} class="search-item row">
+                <div class="col-2">
+                  <Link to={"/items/" + item.item_id}>
+                    <img onClick={() => window.scrollTo(0, 0)} 
+                         class="search-item-img" style={{width: "100%"}} src={item.item_image} alt="Item"></img>
+                  </Link>
+                </div>
+                <div class="col-6">
+                  <Link to={"/items/" + item.item_id}>
+                    <div onClick={() => window.scrollTo(0, 0)}>{item.item_name}</div>
+                  </Link>
+                  <div>{item.description}</div>
+                  <div>{this.renderRating(item.average_rating)}</div>
+                </div>
+                <div class="col-1">
+                  <div>{"$" + item.price}</div>
+                </div>
+                <div class="col-2">
+                  <CartAddButton item={item} cls={"addtocart-btn-lg"}/>
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      );
+    }
   }
 }
 
