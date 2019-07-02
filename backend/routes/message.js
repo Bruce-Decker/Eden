7771ae4d38e0fbc2d9f01d839abe79422350c573
Bruce Decker <bruce.d008@gmail.com>
2019-07-01 18:48:59 -0700

    refactor message APIs

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -1,20 +1,32 @@
 const express = require('express')
 const router = express.Router()
 const Message = require('../schema/Message')
+const uuidv4 = require('uuid/v4');
+
 
 router.post('/sendMessage', (req, res) => {
-    var subject = req.body.subject
-    var message = req.body.message
+    var message_id = uuidv4()
     var sender_email = req.body.sender_email
     var receiver_email = req.body.receiver_email
+    var subject = req.body.subject
+    var message = req.body.message
+    var isRead = false
+    var isStarred = false
+    var isTrashed = false
+    var isDeleted = false
     var time = Date.now()
     
     var data = {
-        sender_email,
-        receiver_email,
-        subject,
-        message,
-        time
+       message_id,
+       sender_email,
+       receiver_email,
+       subject,
+       message,
+       isRead,
+       isStarred,
+       isTrashed,
+       isDeleted,
+       time
     }
 
     Message.create(data, function(err, newlyCreated) {
@@ -28,7 +40,7 @@ router.post('/sendMessage', (req, res) => {
 })
 
 
-router.get('/getReceivedMessages/:receiver_email', function(req, res) {
+router.get('/getInboxMessages/:receiver_email', function(req, res) {
     var receiver_email = req.params.receiver_email
     console.log(receiver_email)
     Message.find({receiver_email: receiver_email})
@@ -46,6 +58,51 @@ router.get('/getSentMessages/:sender_email', function(req, res) {
     var sender_email = req.params.sender_email
     
     Message.find({sender_email: sender_email})
+     .sort({'time': 'desc'})
+     .exec(function(err, docs) {
+        if (docs) {
+            res.send(docs)
+        } else {
+            res.send({})
+        }
+    });
+})
+
+router.get('/getStarredMessages/:sender_email', function(req, res) {
+    var sender_email = req.params.sender_email
+    
+    Message.find({sender_email: sender_email, isStarred: true})
+     .sort({'time': 'desc'})
+     .exec(function(err, docs) {
+        if (docs) {
+            res.send(docs)
+        } else {
+            res.send({})
+        }
+    });
+})
+
+
+
+router.get('/getDraftedMessages/:sender_email', function(req, res) {
+    var sender_email = req.params.sender_email
+    
+    Message.find({sender_email: sender_email, isDraft: true})
+     .sort({'time': 'desc'})
+     .exec(function(err, docs) {
+        if (docs) {
+            res.send(docs)
+        } else {
+            res.send({})
+        }
+    });
+})
+
+
+router.get('/getTrashedMessages/:sender_email', function(req, res) {
+    var sender_email = req.params.sender_email
+    
+    Message.find({sender_email: sender_email, isTrashed: true})
      .sort({'time': 'desc'})
      .exec(function(err, docs) {
         if (docs) {
--- backend/schema/Message.js
+++ backend/schema/Message.js
@@ -2,6 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
+    message_id: {
+       type: String,
+       required: true
+    },
     sender_email: {
         type: String,
         required: true
@@ -18,6 +22,46 @@ const MessageSchema = new Schema({
         type: String,
         required: true
     },
+    isRead: {
+        type: Boolean,
+        required: true
+    },
+    isStarred: {
+        type: Boolean,
+        required: true
+    },
+    isTrashed: {
+        type: Boolean,
+        required: true
+    },
+    isDeleted: {
+        type: Boolean,
+        required: true
+    },
+    isDraft: {
+        type: Boolean,
+        required: true
+    },
+    replies: [
+        {
+            name: {
+                type: String,
+                required: true
+            },
+            email: {
+                type: String,
+                required: true
+            },
+            message: {
+                type: String,
+                required: true
+            },
+            time: {
+                type: Date,
+                required: true
+            },
+        }
+    ],
     time: {
         type: Date,
         required: true
