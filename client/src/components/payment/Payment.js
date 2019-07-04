0b1992f6991302487e3f0ab8161e4a7dcd0b2847
Animesh Grover <animesh70@gmail.com>
2019-07-04 12:41:11 -0700

    Added Stripe components

--- client/src/components/payment/Payment.js
+++ client/src/components/payment/Payment.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import {CardElement, Elements, injectStripe, StripeProvider} from 'react-stripe-elements';
+import StripePayment from './StripePayment';
 //import './Payment.css';
 //import {Pay} from './pay.js';
 import RegularBanner from '../banner/RegularBanner';
@@ -9,13 +11,23 @@ import { withRouter } from 'react-router-dom';
 class Payment extends Component {
     constructor(props) {
         super(props);
+        this.submit = this.submit.bind(this);
+    }
+
+    async submit(ev) {
+        // User clicked submit
     }
 
     render() {
         return (
-            <div>
-                <span>Payment Page!!! - Work In Progress</span>
-            </div>
+            <StripeProvider apiKey="pk_test_TYooMQauvdEDq54NiTphI7jx">
+                <div className="example">
+                    <h1>Payment</h1>
+                    <Elements>
+                        <StripePayment />
+                    </Elements>
+                </div>
+            </StripeProvider>
         );
     }
 
