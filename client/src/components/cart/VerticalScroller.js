8b8e0065f7829d9615bf33f519dcdd391be70fc7
Matthew Kwong <mtkwong91@gmail.com>
2019-07-06 12:22:36 -0700

    search result styling

--- client/src/components/cart/VerticalScroller.js
+++ client/src/components/cart/VerticalScroller.js
@@ -42,7 +42,7 @@ class VerticalScroller extends Component {
                     <Link to={"/items/" + rec.id}>
                       <span className={this.props.keyPrefix+"-title"} onClick={() => window.scrollTo(0, 0)}>{rec.name}</span>
                     </Link>
-                    <div className={this.props.keyPrefix+"-price"}>{'$'+rec.price}</div>
+                    <div className={this.props.keyPrefix+"-price"}>{'$'+rec.price.toFixed(2)}</div>
                     <StarRatings
                       rating={rec.avg_rating ? rec.avg_rating : 4+Math.random()}
                       starRatedColor='rgb(40,167,69)'
