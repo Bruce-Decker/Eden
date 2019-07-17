65202b234dd851120c8afa37ab0eaf1ccebdb794
Animesh Grover <animesh70@gmail.com>
2019-07-17 16:13:17 -0700

    updated order schema

--- backend/schema/Order.js
+++ backend/schema/Order.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const OrderSchema = new Schema({
-    item_id: {
-        type: String,
-        required: true
-    },
-    item_name: {
+    order_id: {
         type: String,
         required: true
     },
@@ -14,22 +10,53 @@ const OrderSchema = new Schema({
         type: String,
         required: true
     },
-    user_name: {
-        type: String,
-        required: true
-    },
-    credit_card_number: {
-        type: String,
-        required: true
-    },
     price: {
         type: Number,
         required: true
     },
-    quantity: {
-        type: Number,
-        required: true
+    items: [{
+        item_id: {
+            type: String,
+            required: true
+        },
+        item_name: {
+            type: String,
+            required: true
+        },
+        item_image: {
+            type: String,
+            required: false
+        },
+        description: {
+            type: String,
+            required: false
+        },
+        price: {
+            type: Number,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        bid_price: Number
+    }],
+    tracking_id: {
+        type: String,
+        required: false
+    },
+    payment_id: {
+        type: String,
+        required: false
+    },
+    payment_receipt_url: {
+        type: String,
+        required: false
+    },
+    status: {
+        type: String,
+        reuired: false
     }
-})
+});
 
 module.exports = Order = mongoose.model('order', OrderSchema);
