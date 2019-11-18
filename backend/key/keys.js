d947e7d9242dd00deb4c9e16efc2658cb984667f
Animesh Grover <animesh70@gmail.com>
2019-11-18 13:14:04 -0800

    Removed API keys

--- backend/key/keys.js
+++ backend/key/keys.js
@@ -1,7 +1,7 @@
 module.exports = {
     secretOrKey: 'secret',
-    sendgridApiKey: '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',
-    stripeApiKey: 'sk_test_XipJxj5aXqXxdWljrsx5PKKn00MHy6FWuY',
+    sendgridApiKey: '',
+    stripeApiKey: '',
     shipStationMockServer: 'https://private-anon-e09ceed5a7-shipstation.apiary-mock.com',
     shipStationServer: 'https://ssapi.shipstation.com'
 };
